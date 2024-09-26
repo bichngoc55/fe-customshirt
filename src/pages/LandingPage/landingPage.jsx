7e96a44d772442e255fccc853398879d2ad3d18c
Gấu <115764988+bichngoc55@users.noreply.github.com>
2024-09-27 00:42:11 +0700

    navbar

---
+++ src/pages/LandingPage/landingPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./landingPage.css";
+import { Box } from "@mui/material";
+// import { NavBar } from "../../components/NavBar/navBar";
+function LandingPage() {
+  return (
+    <div className="container">
+      <div className="">Hello</div>
+    </div>
+  );
+}
+
+export default LandingPage;
