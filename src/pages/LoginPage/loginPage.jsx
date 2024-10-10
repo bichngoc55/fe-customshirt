3e9d755baca892af132b1f953fc3aca03778b883
Gấu <115764988+bichngoc55@users.noreply.github.com>
2024-10-11 01:39:24 +0700

    login + navbar + register

--- src/pages/LoginPage/loginPage.jsx
+++ src/pages/LoginPage/loginPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Box, TextField, Button, Typography } from "@mui/material";
+import { Box, Typography, useMediaQuery } from "@mui/material";
 import "./loginPage.css";
 import InputForm from "../../components/inputForm/inputForm";
 import BtnComponent from "../../components/btnComponent/btnComponent";
@@ -8,82 +8,114 @@ import { useNavigate } from "react-router-dom";
 const LoginPage = () => {
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
-  const [isEmailFocused, setEmailFocus] = useState(false);
-  const [isPasswordFocused, setPasswordFocus] = useState(false);
   const [password, setPassword] = useState("");
-  const handleLoginClick = () => {
-    console.log("heheh");
-  };
+  const [loginStatus, setLoginStatus] = useState(null);
+  const isMobile = useMediaQuery("(max-width:768px)");
+
+  const handleLoginClick = () => {};
+
   return (
-    <div className="container" style={{}}>
+    <Box
+      className="container"
+      sx={{
+        position: "relative",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        overflow: "hidden",
+      }}
+    >
       <img
         src={require("../../assets/images/image.png")}
         alt="Decorative"
         style={{
           position: "absolute",
-          top: 0,
-          left: 0,
-          marginTop: "5%",
-          height: "100%",
-          width: "100%",
-          objectFit: "contain",
+          left: "50%",
+          top: "45%",
+          transform: "translate(-50%, -50%)",
+          width: "85%",
+          height: "80%",
+          objectFit: "cover",
           zIndex: -1,
+          borderRadius: "100px",
         }}
       />
       <Box
         display="flex"
-        flexDirection="row"
+        flexDirection={isMobile ? "column" : "row"}
         alignItems="center"
         justifyContent="center"
-        zIndex={1}
         sx={{
-          height: "100%",
           width: "100%",
+          height: "100%",
+          padding: isMobile ? "20px" : "40px",
+          boxSizing: "border-box",
         }}
-        position="relative"
       >
-        <Box className="right">
-          <Typography
-            style={{
-              color: "white",
-              fontFamily: "Montserrat",
-              fontSize: "38px",
-              fontWeight: "lighter",
+        {!isMobile && (
+          <Box
+            className="right"
+            sx={{
+              flex: 1,
+              padding: "20px",
+              display: "flex",
+              flexDirection: "column",
+              marginBottom: "10%",
+              alignItems: "center",
+              justifyContent: "center",
             }}
           >
-            Welcome back to our page{" "}
-          </Typography>
-          <Typography
-            style={{
-              color: "white",
-              fontFamily: "Montserrat",
-            }}
-          >
-            Do not have an account?{" "}
-            <span className="signup-link" onClick={() => navigate("/register")}>
-              {" "}
-              Sign up{" "}
-            </span>
-            now
-          </Typography>
-        </Box>
+            <Typography
+              sx={{
+                color: "white",
+                fontFamily: "Montserrat",
+                fontSize: "clamp(24px, 5vw, 38px)",
+                fontWeight: "lighter",
+                paddingLeft: "5%",
+              }}
+            >
+              Welcome back to our page!
+            </Typography>
+            <Typography
+              sx={{
+                color: "white",
+                fontFamily: "Montserrat",
+                marginTop: "20px",
+                textAlign: "center",
+                fontSize: "clamp(14px, 2vw, 16px)",
+              }}
+            >
+              Do not have an account?{" "}
+              <span
+                className="signup-link"
+                onClick={() => navigate("/register")}
+                style={{ cursor: "pointer" }}
+              >
+                Sign up
+              </span>{" "}
+              now
+            </Typography>
+          </Box>
+        )}
         <Box
           className="left"
           display="flex"
           flexDirection="column"
           alignItems="center"
           justifyContent="center"
-          height="100%"
-          padding="90px"
-          zIndex={1}
-          position="relative"
+          sx={{
+            flex: 1,
+            padding: isMobile ? "20px" : "40px",
+          }}
         >
           <Typography
-            style={{
+            sx={{
               color: "white",
               fontFamily: "Montserrat",
-              fontSize: "30px",
+              fontSize: "clamp(24px, 4vw, 30px)",
               fontWeight: "bold",
+              marginBottom: "20px",
             }}
           >
             SIGN IN
@@ -93,61 +125,96 @@ const LoginPage = () => {
               display: "flex",
               flexDirection: "column",
               gap: "10px",
-              width: "300px",
-              marginTop: "10%",
+              width: "100%",
+              maxWidth: "300px",
             }}
           >
             <Typography
-              style={{
-                marginLeft: "5%",
+              sx={{
                 color: "white",
                 fontFamily: "Montserrat",
+                fontSize: "clamp(14px, 2vw, 16px)",
+                marginLeft: "4%",
               }}
             >
               Username/Email
             </Typography>
-
             <InputForm
               placeholder="Email"
-              width="300px"
-              height="50px"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              onFocus={() => setEmailFocus(true)}
-              onBlur={() => setEmailFocus(false)}
+              width="100%"
+              height="50px"
             />
             <Typography
-              style={{
-                marginLeft: "5%",
+              sx={{
                 color: "white",
                 fontFamily: "Montserrat",
+                fontSize: "clamp(14px, 2vw, 16px)",
+                marginLeft: "4%",
               }}
-              onFocus={() => setPasswordFocus(true)}
-              onBlur={() => setPasswordFocus(false)}
             >
               Password
             </Typography>
-
             <InputForm
               placeholder="Password"
               type="password"
-              width="300px"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
+              width="100%"
               height="50px"
             />
-            <Box sx={{ marginLeft: "17%" }}>
+            {loginStatus && (
+              <Typography
+                sx={{
+                  color: loginStatus.success ? "green" : "red",
+                  textAlign: "center",
+                  marginTop: "10px",
+                  fontSize: "clamp(12px, 1.5vw, 14px)",
+                }}
+              >
+                {loginStatus.message}
+              </Typography>
+            )}
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                marginTop: "20px",
+              }}
+            >
               <BtnComponent
-                width={200}
+                width="100"
                 height={40}
                 handleClick={handleLoginClick}
                 value={"Login"}
               />
             </Box>
           </form>
+          {isMobile && (
+            <Typography
+              sx={{
+                color: "white",
+                fontFamily: "Montserrat",
+                marginTop: "20px",
+                textAlign: "center",
+                fontSize: "clamp(14px, 2vw, 16px)",
+              }}
+            >
+              Do not have an account?{" "}
+              <span
+                className="signup-link"
+                onClick={() => navigate("/register")}
+                style={{ cursor: "pointer", textDecoration: "underline" }}
+              >
+                Sign up
+              </span>{" "}
+              now
+            </Typography>
+          )}
         </Box>
       </Box>
-    </div>
+    </Box>
   );
 };
 
--- src/pages/RegisterPage/registerPage.jsx
+++ src/pages/RegisterPage/registerPage.jsx
@@ -1,13 +1,236 @@
-import React from "react";
-import { Box } from "@mui/material";
-import "./registerPage.css";
+import React, { useState } from "react";
+import { Box, Typography, useMediaQuery } from "@mui/material";
+import InputForm from "../../components/inputForm/inputForm";
+import BtnComponent from "../../components/btnComponent/btnComponent";
+import { useNavigate } from "react-router-dom";
 function RegisterPage() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [loginStatus, setLoginStatus] = useState(null);
+  const isMobile = useMediaQuery("(max-width:768px)");
+
+  const handleRegisterClick = () => {};
   return (
-    <div className="container">
-      <div className="content">
-        <img src="../../assets/images/image.png" alt="" />
-      </div>
-    </div>
+    <Box
+      className="container"
+      sx={{
+        position: "relative",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        overflow: "hidden",
+      }}
+    >
+      <img
+        src={require("../../assets/images/image.png")}
+        alt="Decorative"
+        style={{
+          position: "absolute",
+          left: "50%",
+          top: "45%",
+          transform: "translate(-50%, -50%)",
+          width: "85%",
+          height: "80%",
+          objectFit: "cover",
+          zIndex: -1,
+          borderRadius: "100px",
+        }}
+      />
+      <Box
+        display="flex"
+        flexDirection={isMobile ? "column" : "row"}
+        alignItems="center"
+        justifyContent="center"
+        sx={{
+          width: "100%",
+          height: "100%",
+          padding: isMobile ? "20px" : "40px",
+          boxSizing: "border-box",
+        }}
+      >
+        {!isMobile && (
+          <Box
+            className="right"
+            sx={{
+              flex: 1,
+              padding: "20px",
+              display: "flex",
+              flexDirection: "column",
+              marginBottom: "10%",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Typography
+              sx={{
+                color: "white",
+                fontFamily: "Montserrat",
+                fontSize: "clamp(24px, 5vw, 38px)",
+                fontWeight: "lighter",
+                paddingLeft: "5%",
+              }}
+            >
+              Welcome first to our page!
+            </Typography>
+            <Typography
+              sx={{
+                color: "white",
+                fontFamily: "Montserrat",
+                marginTop: "20px",
+                textAlign: "center",
+                fontSize: "clamp(14px, 2vw, 16px)",
+              }}
+            >
+              Already have an account?{" "}
+              <span
+                className="signup-link"
+                onClick={() => navigate("/register")}
+                style={{ cursor: "pointer" }}
+              >
+                Sign in
+              </span>{" "}
+              now
+            </Typography>
+          </Box>
+        )}
+        <Box
+          className="left"
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+          sx={{
+            flex: 1,
+            padding: isMobile ? "20px" : "40px",
+          }}
+        >
+          <Typography
+            sx={{
+              color: "white",
+              fontFamily: "Montserrat",
+              fontSize: "clamp(24px, 4vw, 30px)",
+              fontWeight: "bold",
+              marginBottom: "20px",
+            }}
+          >
+            SIGN UP
+          </Typography>
+          <form
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              //   gap: "5px",
+              width: "100%",
+              maxWidth: "300px",
+              marginBottom: "50px",
+            }}
+          >
+            <Typography
+              sx={{
+                color: "white",
+                fontFamily: "Montserrat",
+                fontSize: "clamp(14px, 2vw, 16px)",
+                marginLeft: "4%",
+              }}
+            >
+              Username
+            </Typography>
+            <InputForm
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              width="100%"
+              height="50px"
+            />
+            <Typography
+              sx={{
+                color: "white",
+                fontFamily: "Montserrat",
+                fontSize: "clamp(14px, 2vw, 16px)",
+                marginLeft: "4%",
+              }}
+            >
+              Email
+            </Typography>
+            <InputForm
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              width="100%"
+              height="50px"
+            />
+            <Typography
+              sx={{
+                color: "white",
+                fontFamily: "Montserrat",
+                fontSize: "clamp(14px, 2vw, 16px)",
+                marginLeft: "4%",
+              }}
+            >
+              Password
+            </Typography>
+            <InputForm
+              placeholder="Password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              width="100%"
+              height="50px"
+            />
+            {loginStatus && (
+              <Typography
+                sx={{
+                  color: loginStatus.success ? "green" : "red",
+                  textAlign: "center",
+                  marginTop: "10px",
+                  fontSize: "clamp(12px, 1.5vw, 14px)",
+                }}
+              >
+                {loginStatus.message}
+              </Typography>
+            )}
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                marginTop: "20px",
+              }}
+            >
+              <BtnComponent
+                width="100"
+                height={40}
+                handleClick={handleRegisterClick}
+                value={"Sign up"}
+              />
+            </Box>
+          </form>
+          {isMobile && (
+            <Typography
+              sx={{
+                color: "white",
+                fontFamily: "Montserrat",
+                marginTop: "20px",
+                textAlign: "center",
+                fontSize: "clamp(14px, 2vw, 16px)",
+              }}
+            >
+              Do not have an account?{" "}
+              <span
+                className="signup-link"
+                onClick={() => navigate("/register")}
+                style={{ cursor: "pointer" }}
+              >
+                Sign up
+              </span>{" "}
+              now
+            </Typography>
+          )}
+        </Box>
+      </Box>
+    </Box>
   );
 }
 
